3bac1fe50132078566f414443c7b34f0eacac064
Nadeesha Kalhara <jankalhara@students.nsbm.ac.lk>
2026-06-24 23:39:51 +0530

    edited

---
+++ src/main/java/com/example/autolink_web/AutolinkWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.autolink_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutolinkWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutolinkWebApplication.class, args);
+	}
+
+}
